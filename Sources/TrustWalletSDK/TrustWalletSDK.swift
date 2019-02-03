f79d10916f8006a1d26956e5104ad97005a63869
liweiz <matt.z.lw@gmail.com>
2019-02-03 14:24:28 -0500

    corrected type

--- Sources/TrustWalletSDK/TrustWalletSDK.swift
+++ Sources/TrustWalletSDK/TrustWalletSDK.swift
@@ -109,10 +109,10 @@ public final class TrustWalletSDK {
             return false
         }
         
-        guard let shardingFlag = components.queryParameterValue(for: "shardingFlag").flatMap({ UInt64($0) }) else {
+        guard let shardingFlag = components.queryParameterValue(for: "shardingFlag").flatMap({ BigInt($0) }) else {
             return false
         }
-        guard let systemContract = components.queryParameterValue(for: "systemContract").flatMap({ UInt64($0) }) else {
+        guard let systemContract = components.queryParameterValue(for: "systemContract").flatMap({ BigInt($0) }) else {
             return false
         }
         guard let via = components.queryParameterValue(for: "via").flatMap({ MoacAddress(string: $0) }) else {
